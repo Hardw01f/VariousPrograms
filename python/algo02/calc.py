b10cd3c5ea0d5c57212e10548afb0f4678ed135b
tsukayam ryo <e155728@RsMacBookPro.local>
2017-06-07 17:14:32 +0900

    send new python program

---
+++ python/algo02/calc.py
@@ -0,0 +1,15 @@
+
+import main
+import sys
+import tool
+
+array1=[8,1,-1,2]
+array2=[1,2,-3,-1]
+array3=[0,-1,-1,-1]
+
+JUDGE=1
+
+while JUDGE:
+    ans=main.pibot_calcurate(array1,array2,array3)
+    print(ans)
+
