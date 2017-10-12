e73f3daa0a6f291ea6fe33edad06df548b9f768e
RYO Tsukayama <e155728@ie.u-ryukyu.ac.jp>
2017-10-12 19:49:39 +0900

    fix private method

--- api/coincheck/ticker/coincheckapi_nowrate.py
+++ api/coincheck/ticker/coincheckapi_nowrate.py
@@ -31,10 +31,10 @@ def select(result):
 #slackにpost_apiを叩くメソッド
 def send_slack(FLAG,now_rate,result):
     if FLAG == 1:
-        cmd = "curl -XPOST -d token=xoxp-211441493619-211441493731-249801662402-198194d20ffa667eb2d4515b42166f50 -d channel=#btc_channel -d text=高騰中:Now_changing_rate_:__now_rate:" + str(now_rate) + "__:value_change:" + str(result) + " -d username=btc_alert https://slack.com/api/chat.postMessage"
+        cmd = "curl -XPOST -d token=取得したtoken -d channel=#btc_channel -d text=高騰中:Now_changing_rate_:__now_rate:" + str(now_rate) + "__:value_change:" + str(result) + " -d username=btc_alert https://slack.com/api/chat.postMessage"
         subprocess.call( cmd.split(" ") )
     elif FLAG == 0:
-        cmd = "curl -XPOST -d token=xoxp-211441493619-211441493731-249801662402-198194d20ffa667eb2d4515b42166f50 -d channel=#btc_channel -d text=暴落中:Now_changing_rate_:__now_rate:" + str(now_rate) + "__:value_change:" + str(result) + " -d username=btc_alert https://slack.com/api/chat.postMessage"
+        cmd = "curl -XPOST -d token=取得したtoken -d channel=#btc_channel -d text=暴落中:Now_changing_rate_:__now_rate:" + str(now_rate) + "__:value_change:" + str(result) + " -d username=btc_alert https://slack.com/api/chat.postMessage"
         subprocess.call( cmd.split(" ") )
     else:
         print("Nothing alert")
