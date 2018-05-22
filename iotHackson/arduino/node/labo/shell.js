5a5719c6921a61e9577286bd04658604aa4d79f5
RYO Tsukayama <e155728@ie.u-ryukyu.ac.jp>
2018-05-23 01:30:14 +0900

    trim test

--- iotHackson/arduino/node/labo/shell.js
+++
@@ -1,6 +0,0 @@
-const exec = require('child_process').exec;
-
-exec('ls -la ./',(err,stdout, stderr) => {
-    if(err){ console.log(err);}
-    console.log(stdout);
-});
--- iotHackson/golang/shut_server.sh
+++
@@ -1,1 +0,0 @@
-ps aux | grep "go run server.go" | grep -v grep | awk '{ print "kill -9", $2 }' | sh
